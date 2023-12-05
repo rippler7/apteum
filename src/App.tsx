77fce059b9abfa976298180d7741a1cf3f9b439b
Jeremy Ancog <jancog@apteum.com>
2023-12-05 16:01:01 +0800

    some css declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import NavBar from './components/NavBar'
-import apteumLogo from '/Apteum_logo_RGB.svg'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <div className='navbarArea w-full h-screen'>
-        <div className='navbarArea w-full h-screen'><NavBar /></div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={apteumLogo} className="logo" alt="Apteum logo" />
-        </a>
-      </div>
-      <h1>Looking for an opportunity to expand?</h1>
+    <div>
+    <NavBar />
+      <div className='w-full h-screen'>
+        
       
+      <h1>Looking for an opportunity to expand?</h1>
+      </div>
       <p className="read-the-docs">
         Click on the logo to learn more
       </p>
-    </>
+    </div>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,27 @@
+import apteumLogo from '../assets/Apteum_logo_RGB.svg'
+import {
+    Navbar,
+    MobileNav,
+    Typography,
+    Button,
+    IconButton
+} from '@material-tailwind/react'
 function NavBar(){
     return(
-        <h1>Nav Bar</h1>
-    );
+        <Navbar>
+            <div className='flex-wrap justify-between'>
+                <div>
+                    <a>link</a>
+                    </div>
+                    <div>
+                    <img src={apteumLogo} height={20} width={100} alt=""  />
+                    </div>
+                    <div>
+                    <a>link</a>
+                </div>
+            </div>
+        </Navbar>
+    )
 }
 
 export default NavBar
