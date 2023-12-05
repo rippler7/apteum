4550f75f04120c27e26428f73a6a2ea6353a5639
Jeremy Ancog <jancog@apteum.com>
2023-12-05 13:59:16 +0800

    more base structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import NavBar from './components/NavBar'
 import apteumLogo from '/Apteum_logo_RGB.svg'
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>
+      <div className='navbarArea w-full h-screen'>
+        <div className='navbarArea w-full h-screen'><NavBar /></div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={apteumLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={apteumLogo} className="logo" alt="Apteum logo" />
         </a>
       </div>
       <h1>Looking for an opportunity to expand?</h1>
       
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the logo to learn more
       </p>
     </>
   )
