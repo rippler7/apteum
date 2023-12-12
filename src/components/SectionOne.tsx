5c45817401ba0afa5a92c07452f8263c1ffbde93
Jeremy Ancog <jancog@apteum.com>
2023-12-12 08:19:56 +0800

    fixed content layout and button behavior

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -1,23 +1,27 @@
 import './SectionOne.css';
 function SectionOne(){
     return(
-        <div className='flex w-full justify-between items-center h-full align-items'>
+        <div className='secOneBody flex w-full justify-between items-center h-full align-middle py-10'>
             <div className='heroImg'>
                 &nbsp;
             </div>
-            <div className='flex-col justify-between h-full w-full items-center align-items p-50'>
-                <div className='w-full h-70'>
+            <div className='flex flex-col justify-between h-full w-full items-center align-items p-50'>
+                <div>&nbsp;</div>
+                <div className='w-full py-30'>
                     <h1>Looking to grow?</h1>
                 </div>
-                <div className='tagline w-full px-20 py-30 h-30'>
+                <div className='tagline w-full px-20 py-30'>
                     <p>
                        <h3>Apteum pairs you with professionals abroad to build bespoke staffing solutions</h3>
                     </p>
                 </div>
-                <div className='w-full h-full py-20'>
+                <div className='w-full py-20'>
                     <button>
                         To Expertise
                     </button>
+                </div>
+                <div>
+                    &nbsp;
                 </div>
             </div>
         </div>
