75a5d773c4a33925e3674b1ae2c3ea45396fecb9
Jeremy Ancog <jancog@apteum.com>
2023-12-12 08:24:32 +0800

    fixed centering

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -1,16 +1,15 @@
 import './SectionOne.css';
 function SectionOne(){
     return(
-        <div className='secOneBody flex w-full justify-between items-center h-full align-middle py-10'>
+        <div className='secOneBody flex w-full justify-between items-center h-full py-10'>
             <div className='heroImg'>
                 &nbsp;
             </div>
-            <div className='flex flex-col justify-between h-full w-full items-center align-items p-50'>
-                <div>&nbsp;</div>
-                <div className='w-full py-30'>
+            <div className='flex flex-col justify-center h-full w-full items-center align-items'>
+                <div className='w-full align-bottom'>
                     <h1>Looking to grow?</h1>
                 </div>
-                <div className='tagline w-full px-20 py-30'>
+                <div className='tagline w-full px-20'>
                     <p>
                        <h3>Apteum pairs you with professionals abroad to build bespoke staffing solutions</h3>
                     </p>
