abae8dcef3b57ea7a56dd8ce07f0826cd0318c7e
Jeremy <jancog@apteum.com>
2023-12-14 04:19:43 +0800

    restored the hover effect on menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ function NavBarContainer(){
         console.log(showSidebar);
     }
     return(
-    <>
+  
             <div id='navigationBar' className='h-15 fixed flex justify-between p-3 w-full'>
                 <div className='px-10'>
                     <a><img src={apteumLogo} width={120} alt=""  /></a>
@@ -18,14 +18,14 @@ function NavBarContainer(){
                 <div className='flex align-right w-15 px-3'>                    
                     <div id="hMenu" onClick={toggleMenu} className={'hmbrgr w-100 align-right'}>
                         <br />
-                    </div>                    
-                    <div id='mSidebar' onClick={toggleMenu} className={'bg-[#DFBEA6] w-35vw p-10 fixed h-full right-0 w-35vw top-0 ${showSidebar ?  "visible" : "invisible" }'}>                        
+                    </div>          
+            
+                    <div id='mSidebar' onClick={toggleMenu} className='${toggleMenu}'>                        
                         <h4>This is a test.</h4>
                     </div>
                 </div>
-            </div>
-            
-    </>    
+            </div>            
+    
     )
 }
 
