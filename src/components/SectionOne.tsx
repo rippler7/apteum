2ff067b90610da77ae1af808504f95eccdc440b0
Jeremy Ancog <jancog@apteum.com>
2023-12-13 11:21:47 +0800

    section one mobile view compatibility

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -1,29 +1,30 @@
 import './SectionOne.css';
+import heroImg from '../assets/DSC09860.jpg'
 function SectionOne(){
     return(
-        <div id="sec1" className='secOneBody flex w-full justify-between items-center h-full pt-10'>
-            <div className='heroImg h-full'>
-                &nbsp;
-            </div>
-            <div className='flex flex-col justify-center h-full items-center align-items'>
-                <div className='w-full align-bottom'>
-                    <h1>Looking to grow?</h1>
+        <section className="secOneBody w-full h-full">
+        <div className="h-full w-full px-6 text-center md:px-12 lg:text-left">
+            <div className="w-100 h-full mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
+            <div className="h-full grid items-center gap-12 lg:grid-cols-2 justify-normal">
+                <div className="h-full w-full">
+                <img src={heroImg}  className="w-full h-full object-cover" alt="" />
                 </div>
-                <div className='tagline w-full px-20'>
+                <div className="w-full text-center">        
                     <p>
-                       <h3>Apteum pairs you with professionals abroad to build bespoke staffing solutions</h3>
+                        <h2>Looking to grow?</h2>
                     </p>
+                    <br />
+                    <p>
+                        <h4 className='px-1.5'>Apteum pairs you with professionals abroad to build bespoke staffing solutions</h4>
+                    </p>
+                    <br />
+                <a className="button mb-2 inline-block bg-primary px-12 pt-4 pb-3.5 text-sm"
+                    data-te-ripple-init data-te-ripple-color="light" href="#!" role="button">Get started</a>
                 </div>
-                <div className='w-full pt-20'>
-                    <button>
-                        To Expertise
-                    </button>
-                </div>
-                <div>
-                    &nbsp;
-                </div>
+            </div>
             </div>
         </div>
+        </section>
     )
 }
 
