0ad1642ecc0c82ecfd2ed9d9350a6e6d57ac66bf
Jeremy Ancog <jancog@apteum.com>
2023-12-19 07:22:17 +0800

    image resource linking issue

--- src/Images.tsx
+++ src/Images.tsx
@@ -1,19 +1,19 @@
 const Images = [
     {
         id:1,
-        photo: '/src/assets/headshots/IMG_6272.JPG',
+        photo: '../assets/headshots/IMG_6272.JPG',
         name: 'Dan Fabro',
         position:'Operations Manager'
     },
     {
         id:2,
-        photo: '/src/assets/headshots/IMG_6270.JPG',
+        photo: '../assets/headshots/IMG_6270.JPG',
         name: 'Joma Malacay',
         position:'Finanace and Admin Support'
     },
     {
         id:3,
-        photo: '/src/assets/headshots/IMG_6267.JPG',
+        photo: '../assets/headshots/IMG_6267.JPG',
         name: 'Anne',
         position:'Data Engineer'
     }
