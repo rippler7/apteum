d8ced050ede6ee4809f74e25de422bd6e676b308
Jeremy Ancog <jancog@apteum.com>
2023-12-19 08:45:13 +0800

    placed photos under main assets folder

--- src/Images.tsx
+++ src/Images.tsx
@@ -1,20 +1,19 @@
-const Dir = '../assets';
 const Images = [
     {
         id:1,
-        photo: Dir+'/headshots/IMG_6272.JPG',
+        photo: '/IMG_6272.JPG',
         name: 'Dan Fabro',
         position:'Operations Manager'
     },
     {
         id:2,
-        photo: Dir+'/headshots/IMG_6270.JPG',
+        photo: '/IMG_6270.JPG',
         name: 'Joma Malacay',
         position:'Finanace and Admin Support'
     },
     {
         id:3,
-        photo: Dir+'/headshots/IMG_6267.JPG',
+        photo: '/IMG_6267.JPG',
         name: 'Anne',
         position:'Data Engineer'
     }
