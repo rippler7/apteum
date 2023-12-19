cff6498b0662c86f2cd044fc0c673b8c0c4f90fd
Jeremy Ancog <jancog@apteum.com>
2023-12-19 08:50:39 +0800

    update images saved in assets directory

--- src/Images.tsx
+++ src/Images.tsx
@@ -1,19 +1,19 @@
 const Images = [
     {
         id:1,
-        photo: '/IMG_6272.JPG',
+        photo: '../assets/IMG_6272.JPG',
         name: 'Dan Fabro',
         position:'Operations Manager'
     },
     {
         id:2,
-        photo: '/IMG_6270.JPG',
+        photo: '../assets/IMG_6270.JPG',
         name: 'Joma Malacay',
         position:'Finanace and Admin Support'
     },
     {
         id:3,
-        photo: '/IMG_6267.JPG',
+        photo: '../assets/IMG_6267.JPG',
         name: 'Anne',
         position:'Data Engineer'
     }
