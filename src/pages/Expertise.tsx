4b434ceaa613e1b43769d31d81cf555621f43b37
Jeremy Ancog <jancog@apteum.com>
2023-12-21 07:13:36 +0800

    additional content for expertise page

--- src/pages/Expertise.tsx
+++ src/pages/Expertise.tsx
@@ -1,7 +1,7 @@
 import heroImgExp from '../assets/hero-expertise.jpg'
 function Expertise() {
     return (
-        
+        <>
             <section className="relative md:h-screen sm:h-max flex items-center justify-center text-center text-white z-1">
                 <div className="img-docker absolute top-0 left-0 w-full h-full overflow-hidden">
                     <img src={heroImgExp} className="w-full h-full brightness-50 object-cover" />
@@ -12,7 +12,31 @@ function Expertise() {
 </h3></p>
                 </div>
             </section>
-        
+            
+            <div className="relative flex flex-col items-center max-w-screen-xl px-4 mx-auto md:flex-row sm:px-6 p-8">
+            <div className="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pl-10">
+        <div className="relative w-full p-3 rounded  md:p-8">
+            <div className="rounded-lg w-full">
+                <img src="https://picsum.photos/400/300" />
+            </div>
+        </div>
+    </div>
+    <div className="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pr-10">
+        <div className="text-left">
+            <h2
+                className="text-4xl font-extrabold leading-10 tracking-tightsm:text-5xl sm:leading-none md:text-6xl">
+                Technology
+            </h2>
+            <br />
+            <p className="max-w-md mx-auto mt-3 text-lg">
+            We offer exceptional customer service and onboarding, as well as administration and support, both inside and outside of Australian business hours. Proficient in industry-leading CRM tools, such as Salesforce, Hubspot and Monday, our staff can also prepare documentation, such as research, strategy and white papers.
+            </p>
+            
+        </div>
+    </div>
+   
+</div>
+        </>
     )
 }
 
