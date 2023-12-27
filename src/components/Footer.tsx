22146a41691b27a9a2d92f6dde4420cc7ded7707
Jeremy <jancog@apteum.com>
2023-12-27 15:49:42 +0800

    [WEB-15] Formatted

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,17 @@
 import ApteumW from '../assets/Apteum_logo_WHITE.svg'
 import lnIconW from '../assets/icons8-linkedin-100-white.svg'
 import { Link } from 'react-router-dom'
-function Footer (){
-    return(
-        
-            <div id='footerBar' className='grid lg:fixed md:bottom-0 sm:bottom-0 lg:grid-cols-2 bottom-0 mb-auto w-full justify-between items-center text-right align-middle'>
-                <div className='items-right align-middle px-10 py-10 md:align-center'>
-                <Link to="/"><img src={ApteumW} height={50} width={130} alt="" className='items-center'  /></Link>
-                    </div>
-                <div>
+function Footer() {
+    return (
+
+        <div id='footerBar' className='grid lg:fixed md:bottom-0 sm:bottom-0 lg:grid-cols-2 bottom-0 mb-auto w-full justify-between items-center text-right align-middle'>
+            <div className='items-right align-middle px-10 py-10 md:align-center'>
+                <Link to="/"><img src={ApteumW} height={50} width={130} alt="" className='items-center' /></Link>
+            </div>
+            <div>
                 <div className='flex justify-items-end w-full items-center px-10 pt-3 text-right'>
                     <div className='flex w-full justify-end items-end'>
-                    <div className='px-2'>
+                        <div className='px-2'>
                             <Link to="expertise"><a>Expertise</a></Link>
                         </div>
                         <div className='px-2'>
@@ -37,9 +37,9 @@ function Footer (){
                 <div className='w-full items-center text-right px-10 py-3'>
                     <h4>&#64; 2024 Apteum. All Rights Reserved.</h4>
                 </div>
-                </div>
             </div>
-       
+        </div>
+
     )
 }
 
