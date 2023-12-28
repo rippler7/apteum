aa926625abe8970249be28cdbcf757f104497a2c
Francis M <fmacatangay@apteum.com>
2023-12-28 14:58:03 +0800

    WEB-8: change sidebar implementation, WEB-6: fix sidebar links go blank

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import './App.css';
-import './components/Sidebar.css'
-import Home from './pages/Home'
-import Expertise from './pages/Expertise'
-import NoPage from './pages/NoPage'
-import Story from './pages/Story'
-import Footer from './components/Footer'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/NavBar'
+import "./App.css";
+import Home from "./pages/Home";
+import Expertise from "./pages/Expertise";
+import NoPage from "./pages/NoPage";
+import Story from "./pages/Story";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
 
 function App() {
-
   return (
-    <>            
-      <Routes>
-        <Route path="/">
-          <Route path="/" index element={<Home />} />
-          <Route path='expertise' element={<Expertise />} />
-          <Route path='ourstory' element={<Story />} />
-          <Route path='nopage' element={<NoPage />} />
-        </Route>
-      </Routes>    
-      <div className='w-full'><Navbar /></div>  
-      <div className='w-full sm:relative md:absolute'><Footer /></div>
-    </>
+    <>
+      <Navbar />
 
-  )
+      <main className="pt-[80px]">
+        <Routes>
+          <Route path="/">
+            <Route path="/" index element={<Home />} />
+            <Route path="expertise" element={<Expertise />} />
+            <Route path="ourstory" element={<Story />} />
+            <Route path="nopage" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </main>
+
+      <div className="w-full sm:relative md:absolute">
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,66 @@
-import '../components/Sidebar.css'
-const Sidebar = () => {
-    return(
-        <>
-        <div className="mainSidebar top-0 right-0 w-[35vw] p-10 pl-20 fixed h-full">
-        <div className='flex justify-items-end w-full items-center px-10 pt-3 text-right'>
-                    Testdfgdfgdfg
-                </div>
-        </div>
-        </>
-    )
+import { memo } from "react";
+import CloseIcon from "../assets/icons/close-icon.svg";
+import { Link } from "react-router-dom";
+
+interface SidebarProps {
+  showSideBar: boolean;
+  toggleMenu: () => void;
 }
 
-export default Sidebar
+const Sidebar = ({ showSideBar, toggleMenu }: SidebarProps) => {
+  return (
+    <div
+      className={`p-4 sidebar-container w-[490px] h-screen fixed top-0 ${
+        showSideBar ? "sidebar-active" : "sidebar-hidden"
+      }`}
+    >
+      <div className="flex justify-end">
+        <img
+          className="cursor-pointer"
+          src={CloseIcon}
+          alt="menu"
+          width={40}
+          onClick={toggleMenu}
+        />
+      </div>
+
+      <div className="px-[160px] pt-[86px]">
+        <ul className="text-left">
+          <li>
+            <Link to="expertise" className=" hover:opacity-70">
+              <h3>Expertise</h3>
+            </Link>
+          </li>
+          <li>
+            <Link to="ourstory" className=" hover:opacity-70">
+              <h3>Our Story</h3>
+            </Link>
+          </li>
+          <li className="hidden">
+            <Link to="nopage">
+              <h3>Our Experts</h3>
+            </Link>
+          </li>
+          <li className="hidden">
+            <Link to="nopage">
+              <h3>Careers</h3>
+            </Link>
+          </li>
+          <li className="hidden">
+            <Link to="nopage">
+              <h3>FAQ</h3>
+            </Link>
+          </li>
+          <li className="hidden">
+            <Link to="nopage">
+              <h3>Contact</h3>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const MemoSidebar = memo(Sidebar);
+export default MemoSidebar;
