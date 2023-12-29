f80635fd880ba8daa29ac4dce4d54610c58d4389
Jeremy Ancog <jancog@apteum.com>
2023-12-29 16:39:39 +0800

    [WEB-25] fixing image directory referencing

--- src/const/Images.ts
+++ src/const/Images.ts
@@ -1,20 +1,20 @@
-const Dir = '/headshots'
+const Dir = '/img'
 const Images = [
     {
         id:1,
-        photo: Dir+'/IMG_6256.jpg',
+        photo: Dir+'/IMG_6256.JPG',
         name: 'Jerome',
         position:'Data Engineer'
     },
     {
         id:2,
-        photo: Dir+'/IMG_6259.jpg',
+        photo: Dir+'/IMG_6259.JPG',
         name: 'Mitch',
         position:'Customer Support'
     },
     {
         id:3,
-        photo: Dir+'/IMG_6252.jpg',
+        photo: Dir+'/IMG_6252.JPG',
         name: 'Francis',
         position:'Software Engineer'
     }
