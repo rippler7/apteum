b1b52547480dde0b0e3e14b283a50d716755a6d9
Jeremy Ancog <jancog@apteum.com>
2023-12-29 15:58:24 +0800

    [WEB-23] re optimized images

--- src/const/Images.ts
+++ src/const/Images.ts
@@ -2,19 +2,19 @@ const Dir = '/headshots'
 const Images = [
     {
         id:1,
-        photo: Dir+'/IMG_6256.JPG',
+        photo: Dir+'/IMG_6256.jpg',
         name: 'Jerome',
         position:'Data Engineer'
     },
     {
         id:2,
-        photo: Dir+'/IMG_6259.JPG',
+        photo: Dir+'/IMG_6259.jpg',
         name: 'Mitch',
         position:'Customer Support'
     },
     {
         id:3,
-        photo: Dir+'/IMG_6252.JPG',
+        photo: Dir+'/IMG_6252.jpg',
         name: 'Francis',
         position:'Software Engineer'
     }
