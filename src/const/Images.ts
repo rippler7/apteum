8e9b2a1e38f438391739ee9dded32a8fdd59da84
Jeremy Ancog <jancog@apteum.com>
2023-12-29 15:02:09 +0800

    [WEB-23] Replace images on the Experts section of the home page

--- src/const/Images.ts
+++ src/const/Images.ts
@@ -2,21 +2,21 @@ const Dir = '/headshots'
 const Images = [
     {
         id:1,
-        photo: Dir+'/IMG_6272.JPG',
-        name: 'Dan',
-        position:'Operations Manager'
+        photo: Dir+'/IMG_6256.JPG',
+        name: 'Jerome',
+        position:'Data Engineer'
     },
     {
         id:2,
-        photo: Dir+'/IMG_6270.JPG',
-        name: 'Joma',
-        position:'Finance and Admin Support'
+        photo: Dir+'/IMG_6259.JPG',
+        name: 'Mitch',
+        position:'Customer Support'
     },
     {
         id:3,
-        photo: Dir+'/IMG_6267.JPG',
-        name: 'Anne',
-        position:'Data Engineer'
+        photo: Dir+'/IMG_6252.JPG',
+        name: 'Francis',
+        position:'Software Engineer'
     }
 ]
 
