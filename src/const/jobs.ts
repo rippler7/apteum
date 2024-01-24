4a672b2a97f3387729953cd457c0c894aab26709
Jeremy Ancog <jancog@apteum.com>
2024-01-24 12:28:59 +0800

    [WEB-61] updated the jobs list

--- src/const/jobs.ts
+++ src/const/jobs.ts
@@ -1,24 +1,15 @@
 export const joblist = [
     {
         id:1,
-        title: "Data Analyst",
+        title: "Mid/Senior Front-end Developer",
         type:"Full time",
         city:"Cebu City",
         country: "Philippines",
-        desc:"Our services are tailored for small-to-medium size businesses looking to grow the right way. Rather than provide a one-size-fits-all solution, we focus on developing relationships with like-minded clients who align with our values and share our enthusiasm for a more ethical approach to outsourcing.",
-        link:"https://www.jobstreet.com.ph/Apteum-Corp-jobs/in-Central-Visayas"
-    },
-    {
-        id:2,
-        title: "Front End Developer",
-        type:"Full time",
-        city:"Cebu City",
-        country: "Philippines",
-        desc:"As a Front End Developer, you will play a crucial role in analyzing complex datasets, extracting valuable insights, and presenting actionable recommendations to various stakeholders within the organization. You will report directly to the Chief Financial Officer and will collaborate closely with cross-functional teams to provide accurate and timely information that supports strategic decision-making.",
+        desc:"The successful candidate will utilize their front-end development knowledge to develop modern web mapping applications.\n Able to write clean, scalable code that is simultaneously tested and documented.",
         link:"https://www.jobstreet.com.ph/job/72813557?type=standard&ref=search-standalone#sol=bb5c582487a529be11eb07cf4989bee340072da"
     },
     {
-        id:3,
+        id:2,
         title: "Mid/Senior Back-end Developer (Ruby)",
         type:"Full time",
         city:"Cebu City",
@@ -27,7 +18,7 @@ export const joblist = [
         link:"https://www.jobstreet.com.ph/job/72813460?type=standard&ref=search-standalone#sol=a6f9e43067fa9a1f4a59b5aa8058e05def713fb"
     },
     {
-        id:4,
+        id:3,
         title: "DevOps Engineer",
         type:"Full time",
         city:"Cebu City",
@@ -36,7 +27,7 @@ export const joblist = [
         link:"https://www.jobstreet.com.ph/job/72818595?type=standard&ref=search-standalone#sol=dbc9a792d83a58c9f0a28ee85b49908ade232faa"
     },
     {
-        id:5,
+        id:4,
         title: "Lead Software Engineer",
         type:"Full time",
         city:"Cebu City",
@@ -45,7 +36,7 @@ export const joblist = [
         link:"https://www.jobstreet.com.ph/job/72813631?type=standard&ref=search-standalone#sol=44fb3dd554facf82c4bdbeb41cc3965693e26f96"
     },
     {
-        id:6,
+        id:5,
         title: "Marketing Automation Manager",
         type:"Full time",
         city:"Cebu City",
@@ -54,7 +45,7 @@ export const joblist = [
         link:"https://www.jobstreet.com.ph/job/72456680?type=standard&ref=search-standalone#sol=d1481a98a9cd2f44a162d7d58435ceb24058a7bd"
     },
     {
-        id:7,
+        id:6,
         title: "UI/UX Designer",
         type:"Full time",
         city:"Cebu City",
