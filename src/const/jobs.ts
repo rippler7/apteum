2473b9754d70d67fc09766969b4ad20302af96b7
Jeremy Ancog <jancog@apteum.com>
2024-02-14 13:59:56 +0800

    [web-65] updated jobslist

--- src/const/jobs.ts
+++ src/const/jobs.ts
@@ -1,48 +1,57 @@
 export const joblist = [
     {
         id:1,
+        title: "UI/UX Designer",
+        type:"Full time",
+        city:"Cebu City",
+        country: "Philippines",
+        desc:"Are you an experienced UI/UX Designer looking for a new challenge? Do you want to work on impactful products and create magnificent designs for them? We have just the role for you! \nWe are seeking a talented and experienced UI/UX Designer that will help translate business and customer needs to high-quality design solutions using various design and research methodologies to ensure an excellent user experience. As the UI/UX Designer, you will be responsible for the creation, communication, and implementation of our design services. \nAt our company, we value our employees' well-being and work-life balance. That's why we offer a healthy work-life balance. We believe that when our team members are happy and energised, they produce their best work.",
+        link:"https://www.jobstreet.com.ph/job/73616005"
+    },
+    {
+        id:2,
         title: "Mid/Senior Front-end Developer",
         type:"Full time",
         city:"Cebu City",
         country: "Philippines",
         desc:"The successful candidate will utilize their front-end development knowledge to develop modern web mapping applications.\n Able to write clean, scalable code that is simultaneously tested and documented.",
-        link:"https://www.jobstreet.com.ph/job/72813557?type=standard&ref=search-standalone#sol=bb5c582487a529be11eb07cf4989bee340072da"
-    },
-    {
-        id:2,
-        title: "Mid/Senior Back-end Developer (Ruby)",
-        type:"Full time",
-        city:"Cebu City",
-        country: "Philippines",
-        desc:"Able to write clean, scalable code that is at the same time tested and documented.",
-        link:"https://www.jobstreet.com.ph/job/72813460?type=standard&ref=search-standalone#sol=a6f9e43067fa9a1f4a59b5aa8058e05def713fb"
+        link:"https://www.jobstreet.com.ph/job/73723685"
     },
     {
         id:3,
+        title: "Mid/Senior Backend Developer (Ruby)",
+        type:"Full time",
+        city:"Cebu City",
+        country: "Philippines",
+        desc:"Responsibilities:\n Collaborate with other engineers to architect, develop, test, and support technical solutions to achieve business goals. \nWork within a small cross-functional team containing members with different skills and experience. \nDevelop, test, and optimize API-based logic. \nBuild reusable code and libraries for future use. \nImplementing automated testing platforms and unit tests. \nStay updated on the latest changes in Ruby on Rails and other back-end technologies.",
+        link:"https://www.jobstreet.com.ph/job/73576605"
+    },
+    {
+        id:4,
         title: "DevOps Engineer",
         type:"Full time",
         city:"Cebu City",
         country: "Philippines",
-        desc:"The successful candidate will be responsible for the building, scaling, and security of our cloud systems.",
-        link:"https://www.jobstreet.com.ph/job/72818595?type=standard&ref=search-standalone#sol=dbc9a792d83a58c9f0a28ee85b49908ade232faa"
+        desc:"The successful candidate will be responsible for the building, scaling, and security of our cloud systems. Responsibilities: \nCollaborate with other engineers to architect, develop, test, and support technical solutions to achieve business goals. \nWork within a small cross-functional team containing members with different skills and experience. \nDesign and build production quality and secure CI/CD pipelines. \nStay updated on the latest changes in DevOps and security.",
+        link:"https://www.jobstreet.com.ph/job/73575891"
     },
     {
-        id:4,
+        id:5,
         title: "Lead Software Engineer",
         type:"Full time",
         city:"Cebu City",
         country: "Philippines",
-        desc:"Are you an experienced Lead Software Engineer looking for a new challenge? Do you want to work on a single product and lead a team of developers in building high-quality data services? We have just the role for you!\n \n We are seeking a talented and experienced Software Engineering Lead to manage a team of developers and work on the data services for our product.",
-        link:"https://www.jobstreet.com.ph/job/72813631?type=standard&ref=search-standalone#sol=44fb3dd554facf82c4bdbeb41cc3965693e26f96"
+        desc:"Are you an experienced Lead Software Engineer looking for a new challenge? Do you want to work on a single product and lead a team of developers in building high-quality data services? We have just the role for you! \nWe are seeking a talented and experienced Software Engineering Lead to manage a team of developers and work on the data services for our product. This work will need you to be onsite.",
+        link:"https://www.jobstreet.com.ph/job/73576579"
     },
     {
-        id:5,
-        title: "Marketing Automation Manager",
+        id:6,
+        title: "Admin Assistant",
         type:"Full time",
         city:"Cebu City",
         country: "Philippines",
-        desc:"The purpose of this role is to drive audience awareness, engagement and conversion. This role involves all stages of the funnel from acquiring new members, retaining current members and consumers, and by being involved in property conversations.",
-        link:"https://www.jobstreet.com.ph/job/72456680?type=standard&ref=search-standalone#sol=d1481a98a9cd2f44a162d7d58435ceb24058a7bd"
+        desc:"**Must be available to work in our office in IT Park, Cebu** \nOur client is a leading B2B Food company, which take orders of a very wide variety of products on their marketplace. The role includes answering inbound communication, engaging with customers by asking questions regarding their enquiry, assisting them to place an order or in finding a resolution. A range of basic administrative and business support tasks. This will form half the role. \nCommunication, attention to detail and problem-solving skills are crucial for the success of this role. A multi-tasker with excellent time management. \nRequirements are thoroughly documented already, however you will be able to contribute to the continuous improvement of data quality management processes and systems. \nYou will also need to be a high level experienced at PowerPoint and Microsoft Office Suite as there will be other ad hoc tasks, which will make up the other 50% of the role.",
+        link:"https://www.jobstreet.com.ph/job/73387980"
     }
 ];
 
